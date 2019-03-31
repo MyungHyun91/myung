f59272af012fad2948e9f0fd8f9b944b105f9b28
myung <kimchiman91@naver.com>
2019-03-31 22:35:10 +0900

    타일맵 생성

--- myung_engine.20190331.001.js
+++ myung_engine.20190331.001.js
@@ -4,27 +4,40 @@
  * 저자 : kmh
  * 이메일 : kimchiman91@naver.com
  **/
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        alert('mobile');
-    }
-    else{
-        alert('not mobile');
-    }
+
+//var G_B_MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+var canvas = null;
+var ctx = null;
 
 function demo_init()
 {
- 
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        alert('mobile');
-    }
-    else{
-        alert('not mobile');
-    }
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    
+    // 캔버스 초기화
+   // objInterval = setInterval(start, 100);
+
+    var imgTile001 = new Image();
+    
+    imgTile001.addEventListener('load', function(){
+        ctx.drawImage(imgTile001, 0, 0);
+        ctx.drawImage(imgTile001, 32, 0);
+        ctx.drawImage(imgTile001, 64, 0);
+        ctx.drawImage(imgTile001, 96, 0);
+        ctx.drawImage(imgTile001, 128, 0);
+    },false);
+    imgTile001.src='./image/tile_map001.png';
+   
 }
 
 function start()
 {
+       // 모바일이면 화면에 조이스틱을 보여라
+//    if(B_MOBILE){
+//        
+//    }
     
+ 
 }
 
 function end()
