69a96737ab511db85eb8cfbef9248cd4addb4de1
myung <kimchiman91@naver.com>
2019-03-31 21:38:23 +0900

    모바일 확인 추가

---
+++ myung_engine.20190331.001.js
@@ -0,0 +1,38 @@
+/**
+ * 일자 : 2019-03-31
+ * 버전 : 1.20190331.001
+ * 저자 : kmh
+ * 이메일 : kimchiman91@naver.com
+ **/
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        alert('mobile');
+    }
+    else{
+        alert('not mobile');
+    }
+
+function demo_init()
+{
+ 
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        alert('mobile');
+    }
+    else{
+        alert('not mobile');
+    }
+}
+
+function start()
+{
+    
+}
+
+function end()
+{
+    
+}
+
+function create_module()
+{
+    
+}
