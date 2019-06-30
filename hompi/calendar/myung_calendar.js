4720c0e95d25808a919305a924c811752ea9de00
myung <kimchiman91@naver.com>
2019-06-30 17:28:14 +0900

    캘린더 일정추가 자바스크립트로 뺌
    라이브러리로 사용 가능하게 처음부터 개발하야 할 듯

--- hompi/calendar/myung_calendar.js
+++ hompi/calendar/myung_calendar.js
@@ -31,22 +31,48 @@
  */
 function init()
 {
-    alert("init 호출");
-    st1.rto = new Apple();
+    var $div = $('<div>Hello</div>');
+    $("#o1").append($div);
+    
+    // 셀 변수로 쪼개기
+    var nCnt = 0;
+    
+    for(var i=0; i<6; i++){
+        var $divRow = $("<div class='calendar_row'></div>");
+        var $divButton = $("<div id='btn" + nCnt/7 + "' class='btn'></div>"); // btn0 은 0~5
+        $divRow.append($divButton);
+        
+        for(var j=0; j<7; j++){
+      
+            var $divD1 = $("<div class='d1' cnt='" + nCnt + "'></div>"); // cnt는 0~42
+            var $divD2 = $("<div class='d2'>" + nCnt + "</div>");
+            var $divD3 = $("<div class='d3'>몸통</div>");
+            
+            $divD1.append($divD2);
+            $divD1.append($divD3);
+            $divRow.append($divD1);
+            
+            nCnt++;
+        }
+        
+        $("#o1").append($divRow);
+    }
+    
     
 }
 
-var st = {
-    asd : "감자",
-    ddd : "고구마",
-    rto : null
-}
-
-function Apple (type) {
-    this.type = type;
-    this.color = "red";
-}
+/*
+ css 파일은 있고
+ 자바스크립트로 수정할껀데 태그 생성되기전에 수정이 될까?
+ 컨트롤의 css 를 바꾸는거여서 안될거같고
+ css 설정 쉽게 하려면 jquery 쓰자
+ pollock 아이콘을 로딩시간동안 보이기
+  * param : objParent
+  
+  * return : 달력 object 최상당이 div 이니까 클래스 파일을 넘겨줘
  
-Apple.prototype.getInfo = function() {
-    return this.color + ' ' + this.type + ' apple';
-};
+ */
+function createFrame(objParent, clsInfo)
+{
+    
+}
