cc31e90a1132077132f14bef690ea79613f1e94b
myung <kimchiman91@naver.com>
2019-07-02 08:47:00 +0900

    캘린더 디자인 수정중

--- hompi/calendar/myung_calendar.js
+++ hompi/calendar/myung_calendar.js
@@ -8,7 +8,18 @@
  * 상수 선언부
  *****************************************************/
 
+// 캡슐화 어떻게 할꺼야???
 
+// 구조체 예제
+var ST_CALENDAR = {
+    DATE_: null,
+    PARENT_TAG_: null,
+}
+
+var WEEK_TEXT = ["일", "월", "화", "수", "목", "금", "토"];
+
+// 각 객체 가져오는 getter setter 함수를 만들어줘야하고~ 
+// css 로고 구조화는 어떻게 
 
 /*****************************************************
  * 이벤트함수 선언부
@@ -22,6 +33,17 @@
  *****************************************************/
 
 
+// 제이쿼리 로드유무 확인후 로드
+function loadJQuery()
+{
+    if (typeof jQuery == 'undefined') {
+        var script = document.createElement('script');
+        script.type = "text/javascript";
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
+        document.getElementsByTagName('head')[0].appendChild(script);
+    } 
+}
+
 
 /*
  * description : 제이쿼리 엔진을 로드하고 캘린더 태그를 초기화한다.
@@ -31,33 +53,8 @@
  */
 function init()
 {
-    var $div = $('<div>Hello</div>');
-    $("#o1").append($div);
-    
-    // 셀 변수로 쪼개기
-    var nCnt = 0;
-    
-    for(var i=0; i<6; i++){
-        var $divRow = $("<div class='calendar_row'></div>");
-        var $divButton = $("<div id='btn" + nCnt/7 + "' class='btn'></div>"); // btn0 은 0~5
-        $divRow.append($divButton);
-        
-        for(var j=0; j<7; j++){
-      
-            var $divD1 = $("<div class='d1' cnt='" + nCnt + "'></div>"); // cnt는 0~42
-            var $divD2 = $("<div class='d2'>" + nCnt + "</div>");
-            var $divD3 = $("<div class='d3'>몸통</div>");
-            
-            $divD1.append($divD2);
-            $divD1.append($divD3);
-            $divRow.append($divD1);
-            
-            nCnt++;
-        }
-        
-        $("#o1").append($divRow);
-    }
-    
+    // 1. 태그 생성
+    createFrame();
     
 }
 
@@ -72,7 +69,46 @@ function init()
   * return : 달력 object 최상당이 div 이니까 클래스 파일을 넘겨줘
  
  */
-function createFrame(objParent, clsInfo)
+function createFrame()
 {
+    // 셀 변수로 쪼개기
+    var nCnt = 0;
     
+    // 요일 넣어주자~~~~
+    
+    // 날짜 넣어주자~~~~
+    for(var i=0; i<7; i++){
+        
+        if(i==0){
+            var $divRow = $("<div class='calendar_week_row'></div>");
+             for(var j=0; j<7; j++){
+                var $divD0 = $("<div class='d1'>" + WEEK_TEXT[j] + "</div>"); // cnt는 0~42
+                $divRow.append($divD0);
+             }
+            
+            $("#o1").append($divRow);
+        }
+        else{
+            var $divRow = $("<div class='calendar_row'></div>");
+        
+            var $divButton = $("<div id='btn" + nCnt/7 + "' class='btn'></div>"); // btn0 은 0~5
+        $divRow.append($divButton);
+        
+        for(var j=0; j<7; j++){
+      
+            var $divD1 = $("<div class='d1' cnt='" + nCnt + "'></div>"); // cnt는 0~42
+            var $divD2 = $("<div class='d2'>" + nCnt + "</div>");
+            var $divD3 = $("<div class='d3'>몸통</div>");
+            
+            $divD1.append($divD2);
+            $divD1.append($divD3);
+            $divRow.append($divD1);
+            
+            nCnt++;
+        }
+        $("#o1").append($divRow);    
+        }
+        
+        
+    }   
 }
